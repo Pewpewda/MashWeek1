325623d46cfb71d64a2a98f219a25c3e98b39e5a
Ryan Chan <ryan.w.chan@student.uts.edu.au>
2025-02-23 18:42:00 +1100

    Added helicopter movement in a new script

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 2f;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+    private int soldiersPickedUp = 0; // Counter for picked-up soldiers
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0; // No gravity effect
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W)) moveY = 1f;
+        if (Input.GetKey(KeyCode.S)) moveY = -1f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if (moveX < 0) spriteRenderer.flipX = true;
+        else if (moveX > 0) spriteRenderer.flipX = false;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveDirection * moveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tree"))
+        {
+            Debug.Log("Game Over! You hit a tree.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Restart the game
+        }
+        else if (collision.CompareTag("Soldier"))
+        {
+            Debug.Log("Soldier picked up!");
+            soldiersPickedUp++; // Increase the counter
+            Destroy(collision.gameObject); // Remove the soldier from the scene
+        }
     }
 }
